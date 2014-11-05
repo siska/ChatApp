73cea7c594192023289ee7d20e67925f3bde3e05
Ryan Siska <rsiska1@gmail.com>
2014-11-04 18:45:59 -0600

    Initial Commit

---
+++ ChatApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChatApp
+//
+//  Created by S on 11/4/14.
+//  Copyright (c) 2014 Vi & Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
